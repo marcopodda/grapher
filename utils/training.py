dc91b15611d9e193c8d10ac4bf76a8491f144864
Marco Podda <marco.podda@di.unipi.it>
2019-06-15 01:12:48 +0200

    added cpu/gpu support

--- utils/training.py
+++ utils/training.py
@@ -2,5 +2,5 @@ import torch
 
 
 def get_device(config):
-    use_cuda = config.device == "cuda" and torch.cuda.is_available()
+    use_cuda = config.device in ["cuda", "gpu"] and torch.cuda.is_available()
     return torch.device("cuda" if use_cuda else "cpu")

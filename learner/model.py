8cd9f84f56ad016176e5b649dcf7b097c3caf1f5
Marco Podda <marco.podda@di.unipi.it>
2019-06-28 09:06:47 +0200

    modified sampler

--- learner/model.py
+++ learner/model.py
@@ -114,7 +114,7 @@ class Model(nn.Module):
                 continue
 
             samples.append(nx.Graph(edges))
-        print("   sampled", len(samples))
+        print("   sampled", len(samples), "num samples", num_samples)
         return samples
 
 

9a5cae06e1429f9d39ae0c7a1d4fbdb944c8b42b
Marco Podda <marco.podda@di.unipi.it>
2019-07-02 10:53:58 +0200

    fixed confs

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-MODEL_NAMES = ("GRAPHER", "GRAPHRNN", "GRU", "ER_degree", "ER_clustering", "BA_degree", "BA_clustering")
+MODEL_NAMES = ("GRAPHER", "GRAPHRNN", "GRU", "ER_degree", "BA_degree")
 DATASET_NAMES = ("community", "ego", "ladders", "ENZYMES", "PROTEINS_full")
 METRIC_NAMES = ("degree", "clustering", "graphlet")
 ORDER_NAMES = ("bfs-fixed", "random", "smiles")

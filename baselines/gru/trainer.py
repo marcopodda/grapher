b22b097ebf8b7d2580f9c4c85919809b2cfd6750
Marco Podda <marco.podda@di.unipi.it>
2019-07-03 12:40:15 +0200

    fixed confs

--- baselines/gru/trainer.py
+++ baselines/gru/trainer.py
@@ -81,7 +81,7 @@ class GRUTrainer:
             self.best_losses.append(self.best_loss)
             if len(self.best_losses) > 20:
                 best_losses = self.best_losses[-20:]
-                if max(best_losses) - min(best_losses) < 1e-3:
+                if max(best_losses) - min(best_losses) < 1e-4:
                     print("Early stopping")
                     break
 

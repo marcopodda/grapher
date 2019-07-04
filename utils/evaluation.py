409e2d1f06e60cec3cdde414a7c4216401e60280
Marco Podda <marco.podda@di.unipi.it>
2019-07-04 11:51:20 +0200

    modified evaluator

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -55,24 +55,42 @@ def kl_divergence(sample_ref, sample_pred, bin_size):
     return entropy(counts_ref, counts_pred), counts_ref, counts_pred
 
 
-def graphlet_kl(graph_ref, graph_pred):
+def graphlet_kl(ref, target):
+    if isinstance(ref[0], tuple) or isinstance(ref[0], list):
+        ref = [clean_graph(e) for e in ref]
+
+    if isinstance(target[0], tuple) or isinstance(target[0], list):
+        target = [clean_graph(e) for e in target]
+
     bins = 30
-    gl_hist_ref = get_gl_hists(graph_ref, bins=bins)
-    gl_hist_pred = get_gl_hists(graph_pred, bins=bins)
+    gl_hist_ref = get_gl_hists(ref, bins=bins)
+    gl_hist_pred = get_gl_hists(target, bins=bins)
     return kl_divergence(gl_hist_ref, gl_hist_pred, bins)
 
 
-def clustering_kl(graph_ref, graph_pred):
-    clust_hist_ref = get_cc_hists(graph_ref, bins=BINS)
-    clust_hist_pred = get_cc_hists(graph_pred, bins=BINS)
+def clustering_kl(ref, target):
+    if isinstance(ref[0], tuple) or isinstance(ref[0], list):
+        ref = [clean_graph(e) for e in ref]
+
+    if isinstance(target[0], tuple) or isinstance(target[0], list):
+        target = [clean_graph(e) for e in target]
+
+    clust_hist_ref = get_cc_hists(ref, bins=BINS)
+    clust_hist_pred = get_cc_hists(target, bins=BINS)
     return kl_divergence(clust_hist_ref, clust_hist_pred, BINS)
 
 
-def degree_kl(graph_ref, graph_pred):
-    deg_hist_ref = get_dd_hists(graph_ref)
-    deg_hist_pred = get_dd_hists(graph_pred)
-    max_num_ref = max([G.number_of_nodes() for G in graph_ref])
-    max_num_pred = max([G.number_of_nodes() for G in graph_pred])
+def degree_kl(ref, target):
+    if isinstance(ref[0], tuple) or isinstance(ref[0], list):
+        ref = [clean_graph(e) for e in ref]
+
+    if isinstance(target[0], tuple) or isinstance(target[0], list):
+        target = [clean_graph(e) for e in target]
+
+    deg_hist_ref = get_dd_hists(ref)
+    deg_hist_pred = get_dd_hists(target)
+    max_num_ref = max([G.number_of_nodes() for G in ref])
+    max_num_pred = max([G.number_of_nodes() for G in target])
     max_num = max(max_num_ref, max_num_pred)
     return kl_divergence(deg_hist_ref, deg_hist_pred, max_num)
 

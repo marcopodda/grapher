3c52f40ee60e2ec26dceebbb318e0344f1b9ab7c
Marco Podda <marco.podda@di.unipi.it>
2019-07-04 16:41:11 +0200

    fix histo

--- experiment/evaluator.py
+++ experiment/evaluator.py
@@ -112,6 +112,7 @@ class EvaluatorBase:
 
     def evaluate(self):
         for dataset_name in DATASET_NAMES:
+            print(dataset_name)
             result = Result(self.model_name, dataset_name)
             exp = load_experiment(self.root, self.model_name, dataset_name)
             dataset = load_dataset(dataset_name, self.model_name, exp)

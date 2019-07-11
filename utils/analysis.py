3c0e1619f82f885235d024f6f06a91b308e0f5a0
Marco Podda <marco.podda@di.unipi.it>
2019-07-11 12:47:15 +0200

    added lates utilities

--- utils/analysis.py
+++ utils/analysis.py
@@ -1,16 +1,17 @@
 import time
+import numpy as np
 from pathlib import Path
 
 from .serializer import load_yaml
-from .constants import MODEL_NAMES, DATASET_NAMES, ORDER_NAMES
+from .constants import MODEL_NAMES, DATASET_NAMES, ORDER_NAMES, METRIC_NAMES, QUANTITATIVE_METRIC_NAMES
 
 root = Path('RUNS')
 order_root = Path('RUNS') / "ORDER"
-MODEL_NAMES = ["ER", "BA", "GRU", "GRAPHRNN", "GRAPHER"]
-DATASET_NAMES = list(DATASET_NAMES)
+
 
 def to_hms(secs):
     return time.strftime('%H:%M:%S', time.gmtime(secs))
+
 
 def load_result(model_name, dataset_name, order):
     path = root if order is False else order_root
@@ -19,93 +20,58 @@ def load_result(model_name, dataset_name, order):
     return load_yaml(result_path)
 
 
-def process_kld(result, metric):
-    score = result[metric]["mean"]
-    score_std = result[metric]["std"]
-    score = "{:.6f} +/- {:.4f}".format(score, score_std)
-    return score
-
-
-def process_metric(result, model_name, dataset_name, metric_name):
-    names = sorted([name for name in result if metric_name in name])
-
-    metric1 = result[names[0]]
-    metric2 = result[names[1]]
-
-    if metric_name == 'time':
-        metric1 = to_hms(metric1)
-        metric2 = to_hms(metric2)
-        return f"{model_name:20} - {dataset_name:20} - " + \
-           f"{names[0].capitalize()} samples: {metric1} " + \
-           f"{names[1].capitalize()} samples: {metric2}"
-
-    return f"{model_name:20} - {dataset_name:20} - " + \
-           f"{names[0].capitalize()} samples: {float(metric1):.6f} " + \
-           f"{names[1].capitalize()} samples: {float(metric2):.6f}"
-
-
-def klds_by_dataset(dataset_name, metric_name, order=False):
-    klds = []
-    names = MODEL_NAMES if order is False else ORDER_NAMES
-    for model_name in names:
-        try:
-            result = load_result(model_name, dataset_name, order)
-            kld = process_kld(result, metric_name)
-            klds.append(kld)
-        except:
-            if model_name == "smiles":
-                klds.append("--")
-            else:
-                klds.append("0")
-    if order is True:
-        try:
-            model_name = "GRAPHER"
-            result = load_result(model_name, dataset_name, False)
-            kld = process_kld(result, metric_name)
-            klds.append(kld)
-        except:
-            klds.append("0")
-
-    return " & ".join(klds)
-
-
-def klds_by_model(model_name, metric_name, order=False):
-    klds = []
-    for dataset_name in DATASET_NAMES:
-        try:
-            result = load_result(model_name, dataset_name, order)
-            kld = process_kld(result, metric_name)
-            klds.append(kld)
-        except:
-            klds.append("0")
-
-    return r" & ".join(klds)
-
-
-def metric_by_model(model_name, metric_name, order=False):
-    metrics = []
-    for dataset_name in DATASET_NAMES:
-        try:
-            result = load_result(model_name, dataset_name, order)
-            metric = process_metric(result, model_name, dataset_name, metric_name)
-            metrics.append(metric)
-        except:
-            metrics.append("0")
-
-    return " & ".join(metrics)
+def process_result(result, metric_name):
+    if metric_name in METRIC_NAMES:
+        mean = result[metric_name]["mean"]
+        std = result[metric_name]["std"]
+        return mean, std
+    return result[metric_name], None
 
 
 def metric_by_dataset(dataset_name, metric_name, order=False):
-    metrics = []
+    scores, stds = [], []
     names = MODEL_NAMES if order is False else ORDER_NAMES
     for model_name in names:
-        try:
-            result = load_result(model_name, dataset_name, order=order)
-            metric = process_metric(result, model_name, dataset_name, metric_name)
-            metrics.append(metric)
-        except:
-            metrics.append("0")
+        result = load_result(model_name, dataset_name, order=order)
+        score, std = process_result(result, metric_name)
+        scores.append(score)
+        stds.append(std)
 
-    string = " & ".join(metrics)
-    string.replace(r"\\", r"\\")
-    return string
+    return scores, stds
+
+
+def metrics_by_dataset(dataset_name, quantitative=False, order=False):
+    metric_names = METRIC_NAMES if quantitative is False else QUANTITATIVE_METRIC_NAMES
+    scores_all, stds_all = [], []
+    for metric_name in metric_names:
+        scores, stds = metric_by_dataset(dataset_name, metric_name, order=order)
+        scores_all.append(scores)
+        stds_all.append(stds)
+    return scores_all, stds_all
+
+
+def mean_rank(metric_name):
+    def get_scores(model_name, metric_name):
+        scores = []
+
+        for dataset_name in DATASET_NAMES:
+            result = load_result(model_name, dataset_name, order=False)
+            score, _ = process_result(result, metric_name)
+            scores.append(score)
+
+        return scores
+
+    means_mat = []
+    for model_name in MODEL_NAMES:
+        means = get_scores(model_name, metric_name)
+        means_mat.append(means)
+    means_mat = np.array(means_mat)
+    return (np.argsort(means_mat.T).mean(axis=0) + 1).tolist()
+
+
+def mean_ranks(quantitative=False):
+    metric_names = METRIC_NAMES if quantitative is False else QUANTITATIVE_METRIC_NAMES
+    ranks = []
+    for metric_name in metric_names:
+        ranks.append(mean_rank(metric_name))
+    return ranks
--- utils/constants.py
+++ utils/constants.py
@@ -1,6 +1,7 @@
-MODEL_NAMES = ("GRAPHER", "GRAPHRNN", "GRU", "ER", "BA")
+MODEL_NAMES = ("ER", "BA", "GRU", "GRAPHRNN", "GRAPHER")
 DATASET_NAMES = ("ladders", "community", "ego", "ENZYMES", "PROTEINS_full")
 METRIC_NAMES = ("degree", "clustering", "orbit")
+QUANTITATIVE_METRIC_NAMES = ("novelty1000", "novelty5000", "uniqueness1000", "uniqueness5000")
 ORDER_NAMES = ("random", "bfs-random", "smiles")
 
 PAD = 0

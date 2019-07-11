b564a2f04a6fd0d14c250006fb205f58f23edbd9
Marco Podda <marco.podda@di.unipi.it>
2019-07-11 14:04:41 +0200

    fixed plots

--- utils/plots.py
+++ utils/plots.py
@@ -8,7 +8,8 @@ from pathlib import Path
 from learner.trainer import Trainer
 from config.config import Config
 from dataset import get_dataset_class
-
+from .constants import ORDER_NAMES, MODEL_NAMES, DATASET_NAMES
+from .serializer import load_yaml
 
 sns.set('paper')
 sns.set_style('whitegrid')
@@ -22,6 +23,22 @@ HUMANIZE_DATASET = {
     "ego": "Ego"
 }
 
+HUMANIZE_ORDER = {
+    "random": "Random",
+    "bfs-random": "BF Random",
+    "smiles": "SMILES",
+}
+
+HUMANIZE_METRIC = {
+    "degree": "Average Degree Distribution",
+    "clustering": "Average Clustering Coefficient",
+    "orbit": "Average Orbit Count"
+}
+
+
+root = Path('RUNS')
+order_root = Path('RUNS') / "ORDER"
+
 
 def _to_hist_data(v):
     data = []
@@ -30,22 +47,31 @@ def _to_hist_data(v):
     return data
 
 
-def _double_plot(ref, pred, stat_name, model_name, dataset_name):
-    data1 = _to_hist_data(ref)
-    data2 = _to_hist_data(pred)
-    sns.distplot(data1, hist=False, label="Model")
-    ax = sns.distplot(data2, hist=False, label="Data")
+def _double_plot(ref, pred, metric_name, model_name, dataset_name):
+    data1 = _to_hist_data(pred)
+    data2 = _to_hist_data(ref)
+    sns.distplot(data1, hist=False, label="Generated")
+    ax = sns.distplot(data2, hist=False, label="Real")
+    ax.set_title(HUMANIZE_METRIC[metric_name])
+    if metric_name == "degree":
+        ax.set_ylabel(HUMANIZE_DATASET[dataset_name])
     ax.legend()
     fig = plt.gcf()
-    fig.savefig(f"{model_name}_{dataset_name}_{stat_name}.svg")
+    fig.savefig(f"{model_name}_{dataset_name}_{metric_name}.svg")
 
 
-def plot_stat(results, stat_name, model_name, dataset_name):
-    ref_name = f"{stat_name}_count_data"
-    pred_name = f"{stat_name}_count_samples"
-    ref = results[model_name][dataset_name][ref_name]
-    pred = results[model_name][dataset_name][pred_name]
-    _double_plot(ref, pred, stat_name, model_name, dataset_name)
+def load_result(model_name, dataset_name, order):
+    path = root if order is False else order_root
+    path = path / model_name / dataset_name
+    result_path = list(path.glob("*"))[-1] / "results" / f"{dataset_name}.yaml"
+    return load_yaml(result_path)
+
+
+def plot_stat(metric_name, model_name, dataset_name, order=False):
+    result = load_result(model_name, dataset_name, order=order)[metric_name]
+    ref = result['data_hist']
+    pred = result['samples_hist']
+    _double_plot(ref, pred, metric_name, model_name, dataset_name)
 
 
 def plot_training_on_ordering(dataset_name):
@@ -62,7 +88,7 @@ def plot_training_on_ordering(dataset_name):
     plt.plot(loss, label="Default")
 
     path = Path("RUNS") / "ORDER"
-    for order in ["smiles", "bfs-random", "random"]:
+    for order in reversed(ORDER_NAMES):
         try:
             exp_path = list((path / order / dataset_name).glob("*"))[0]
         except IndexError:
@@ -72,7 +98,7 @@ def plot_training_on_ordering(dataset_name):
         dataset = dataset_class(config, exp_path, dataset_name)
         trainer = Trainer.load(config, exp_path, dataset.input_dim, dataset.output_dim, best=True)
         loss = np.array(trainer.losses1) + np.array(trainer.losses2)
-        plt.plot(loss, label=order)
+        plt.plot(loss, label=HUMANIZE_ORDER[order])
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.legend()
@@ -88,7 +114,8 @@ def plot_generated_graph(model_name, dataset_name, layout="spring"):
         return
 
     ref = torch.load(exp_path / "data" / f"{dataset_name}.pt").graphlist
-    pred = torch.load(exp_path / "samples" / "samples_0.pt")
+    samples_path = list((exp_path / "samples").glob("*.pt"))[0]
+    pred = torch.load(samples_path)
 
     G_ref = np.random.choice(ref, size=1)[0]
     mapping = {n: i for (i, n) in enumerate(G_ref.nodes())}
@@ -98,20 +125,24 @@ def plot_generated_graph(model_name, dataset_name, layout="spring"):
     mapping = {n: i for (i, n) in enumerate(G_pred.nodes())}
     G_pred = nx.relabel_nodes(G_pred, mapping)
 
-    fig, axs = plt.subplots(1, 2)
+    fig, axs = plt.subplots(2, 1)
     layout_fn = getattr(nx, f"{layout}_layout")
     nx.draw_networkx(G_ref, ax=axs[0], pos=layout_fn(G_ref), with_labels=False, node_size=10)
     nx.draw_networkx(G_pred, ax=axs[1], pos=layout_fn(G_pred), with_labels=False, node_size=10)
 
-    axs[0].set_ylabel(HUMANIZE_DATASET[dataset_name])
-    axs[0].set_title("Real")
+    axs[0].set_title(HUMANIZE_DATASET[dataset_name])
+
+    axs[0].set_ylabel("Real")
     axs[0].grid(False)
     axs[0].set_xticks([])
     axs[0].set_yticks([])
 
-    axs[1].set_title("Generated")
+    axs[1].set_ylabel("Generated")
     axs[1].set_xticks([])
     axs[1].set_yticks([])
     axs[1].grid(False)
+
+    fig = plt.gcf()
+    fig.set_size_inches(3, 18)
 
     plt.show()

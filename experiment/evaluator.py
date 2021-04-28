5adbfb18bc29bac6e7f0408acc1c71132798c643
Marco Podda <marco.podda@di.unipi.it>
2021-04-29 00:23:40 +0200

    ok

--- experiment/evaluator.py
+++ experiment/evaluator.py
@@ -46,8 +46,8 @@ class EvaluatorBase:
     requires_quantitative = True
     def __init__(self, model_name):
         self.model_name = model_name
-        self.num_samples = 5000
-        self.num_samples_small = 1000
+        self.num_samples = 600
+        self.num_samples_small = 600
         self.num_trials = 1
         self.fast = model_name == "GRAPHER" or model_name in ORDER_NAMES
 
